0ffec828bdf5648137fd9a4013553044ad23c720
Graham Wihlidal <graham@wihlidal.ca>
2018-12-10 07:48:50 +0100

    Link in vulkan library

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ extern crate bindgen;
 extern crate cc;
 
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let mut build = cc::Build::new();
@@ -31,7 +32,32 @@ fn main() {
 
     build.compile("vma_cpp");
 
+    link_vulkan();
     generate_bindings("gen/bindings.rs");
+}
+
+fn link_vulkan() {
+    let target = env::var("TARGET").unwrap();
+    if target.contains("windows") {
+        println!("cargo:rustc-link-lib=dylib=vulkan-1");
+
+        if let Ok(vulkan_sdk) = env::var("VULKAN_SDK") {
+            let mut vulkan_sdk_path = PathBuf::from(vulkan_sdk);
+
+            if target.contains("x86_64") {
+                vulkan_sdk_path.push("Lib");
+            } else {
+                vulkan_sdk_path.push("Lib32");
+            }
+
+            println!(
+                "cargo:rustc-link-search=native={}",
+                vulkan_sdk_path.to_str().unwrap()
+            );
+        }
+    } else {
+        println!("cargo:rustc-link-lib=dylib=vulkan");
+    }
 }
 
 #[cfg(feature = "generate_bindings")]
