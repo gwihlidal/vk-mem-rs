489c45856d4a09e3805872e70b6aa188a3d54df2
Graham Wihlidal <graham@wihlidal.ca>
2018-12-11 04:01:31 +0100

    Some comments and added extra includes for packaging

--- build.rs
+++ build.rs
@@ -17,6 +17,16 @@ fn main() {
     // Linux (pkconfig) and Windows (VULKAN_SDK environment variable).
     build.define("VMA_STATIC_VULKAN_FUNCTIONS", "0");
 
+    // TODO: Add some configuration options under crate features
+    //#define VMA_HEAVY_ASSERT(expr) assert(expr)
+    //#define VMA_USE_STL_CONTAINERS 1
+    //#define VMA_DEDICATED_ALLOCATION 0
+    //#define VMA_DEBUG_MARGIN 16
+    //#define VMA_DEBUG_DETECT_CORRUPTION 1
+    //#define VMA_DEBUG_INITIALIZE_ALLOCATIONS 1
+    //#define VMA_RECORDING_ENABLED 0
+    //#define VMA_DEBUG_MIN_BUFFER_IMAGE_GRANULARITY 256
+
     // Add the files we build
     let source_files = ["wrapper/vma_lib.cpp"];
 
