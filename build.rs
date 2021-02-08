8955d82a44ba7db9788b520fe273f472de91c581
Philippe Renon <philippe_renon@yahoo.fr>
2021-02-08 21:14:12 +0100

    disable VMA_ASSERT when rust assertions are disabled
    also silences some compile warnings
    
    fixes https://github.com/gwihlidal/vk-mem-rs/issues/45

--- build.rs
+++ build.rs
@@ -11,6 +11,10 @@ fn main() {
     build.include("wrapper");
     build.include("wrapper/vulkan");
 
+    // Disable VMA_ASSERT when rust assertions are disabled
+    #[cfg(not(debug_assertions))]
+    build.define("NDEBUG", "");
+
     // We want to use the loader in ash, instead of requiring us to link
     // in vulkan.dll/.dylib in addition to ash. This is especially important
     // for MoltenVK, where there is no default installation path, unlike
