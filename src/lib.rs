86915e3958c0f0e405f29e6753f9fdc9538de8c2
Marijn Suijten <marijns95@gmail.com>
2024-05-23 15:27:29 +0200

    Improve safety docs on `Allocator::new()` (#70)
    Safety documentation typically lives under a (linkable and anchored) header to be easier to discover, rather than crammed on the same line/paragraph as the documentation because of a missing newline and the `Safety:` "announcement" being regular text. Fix that with some markdown.

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,9 @@ unsafe impl Sync for Allocation {}
 
 impl Allocator {
     /// Construct a new `Allocator` using the provided options.
-    /// Safety: [`AllocatorCreateInfo::instance`], [`AllocatorCreateInfo::device`] and
+    ///
+    /// # Safety
+    /// [`AllocatorCreateInfo::instance`], [`AllocatorCreateInfo::device`] and
     /// [`AllocatorCreateInfo::physical_device`] must be valid throughout the lifetime of the allocator.
     pub unsafe fn new(create_info: AllocatorCreateInfo) -> VkResult<Self> {
         unsafe extern "system" fn get_instance_proc_addr_stub(
